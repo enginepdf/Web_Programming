ed6389d9cd2989f1da121fc7fe1f5272f292200b
engineve <yeindap@gmail.com>
2020-10-05 19:28:11 +0900

    UPDATE models.py

--- Django/book/booktest/models.py
+++ Django/book/booktest/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Arthur(models.Model):
+    name =  models.CharField(max_length=200)
+
+
+class Book(models.Model):
+    id=models.ManyToManyField(Arthur, on_delete=models.CASCADE, primary_key=True)
+    title = models.CharField(max_length=200)
+    release=models.DateTimeField(auto_now_add=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+
+class Publisher(models.Model):
+    name =  models.CharField(max_length=200)
