8c85753a71c98e681b8a3ce86ec877958c40e920
engineve <yeindap@gmail.com>
2020-10-28 10:25:58 +0900

    Create flashTest.js
    flash test

---
+++ Node.js/flashTest.js
@@ -0,0 +1,31 @@
+var express = require('express')
+var session = require('express-session')
+var bodyParser=requier('body-parser');
+var flash=require('connect-flash');
+
+var app = express();
+
+let PORT=8900;
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(session({
+    secret: 'sessionsecret',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+app.use(flash());
+
+app.get('/flash', function (req, res){
+    req.flash('item', 'trasfer')
+    res.send('done')
+})
+
+app.get('/check', function (req, res, next){
+    res.send({messages:req.flash('item')});
+})
+
+app.listen(PORT, () => {
+    console.log(`server listen on ${PORT}`);
+  });
