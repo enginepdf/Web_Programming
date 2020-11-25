b34eca9bb6e9d5c29c70557d7eacddc3fe82c26d
engineve <yeindap@gmail.com>
2020-11-25 23:38:55 +0900

    UPDATE script.js ADD index.html
    attr, .val(), .prop() etc

--- jQuery/script.js
+++ jQuery/script.js
@@ -3,4 +3,12 @@ $(function(){
     return index%2===0;   // even
 }).css("background-color", "rgba(100,100,100,0.5)");
 
+var link = $("#link");
+console.log(link.attr("href")); // /test
+link.attr("href", "www.google.com");
+
+var email = $("input[type=email]").val();
+console.log(email);  // email here
+
+console.log(link.prop("checked")); // undefined
 });
