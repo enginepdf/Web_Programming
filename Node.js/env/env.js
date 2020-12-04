7ddf13b139e894532b1352b08e1e186e46e6553a
engineve <yeindap@gmail.com>
2020-12-04 13:22:08 +0900

    CREATE package.json
    process.env variables

--- Node.js/env/env.js
+++ Node.js/env/env.js
@@ -3,3 +3,4 @@ const express=require('express');
 if(process.env.NODE_ENV!=='production') require('dotenv').config();
 
 console.log(process.env.secretkey); // secret
+console.log(process.env.NODE_ENV);
