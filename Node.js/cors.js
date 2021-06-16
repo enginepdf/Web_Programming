6bf9945f5535970478560aa2cc76d14ed9aa27bd
enginepdf <yeindap@gmail.com>
2021-06-17 05:00:20 +0900

    Create cors.js
    cors as a middleware in node.js

---
+++ Node.js/cors.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+var corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    // optionsSuccessStatus: 200,
+    methods : ['GET', 'PUT', 'POST']  // 'GET,PUT,POST'
+  }
+
+// app.use(cors(corsOptions))
+
+// app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
+// })
